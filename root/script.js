a57ed682ae3b0ebfdf4265d7213fc3c854d43255
bradleyaidanjohnson <bradleyaidanjohnson@gmail.com>
2024-01-06 11:25:38 +0000

    working javascript, do html and css for front end

--- root/script.js
+++ root/script.js
@@ -3,13 +3,19 @@ const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
 const resultReturn = (str) => {
-  const reversedString = str
+  const reNonAlpha = /[^a-zA-Z\d]/g;
+  const strAplhanumOnly = str.replace(reNonAlpha, '');
+  console.log(strAplhanumOnly);
+  
+  const reversedString = strAplhanumOnly
     .split("")
     .reverse()
     .join("");
 
-    if (str === reversedString) {
+    if (strAplhanumOnly.toLowerCase() === reversedString.toLowerCase()) {
       result.innerText = `${str} is a palindrome.`
+    } else {
+      result.innerText = `${str} is not a palindrome.`
     }
 };
 
